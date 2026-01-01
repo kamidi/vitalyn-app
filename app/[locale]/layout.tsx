acd58cce9793ae35f717c1f723ff4eb839ff066f
top mac <topmac@192.168.1.2>
2026-01-01 04:34:41 +0100

    Fix locale layout params typing

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,15 +1,14 @@
-import "../globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
+import "../globals.css";
 
-export default async function LocaleLayout({
-  children,
-  params,
-}: {
+type Props = {
   children: React.ReactNode;
-  params: { locale: string };
-}) {
-  const { locale } = params;
+  params: Promise<{ locale: string }>;
+};
+
+export default async function LocaleLayout({ children, params }: Props) {
+  const { locale } = await params;
 
   const messages = await getMessages({ locale });
 
