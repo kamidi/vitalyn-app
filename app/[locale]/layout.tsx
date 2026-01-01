e7b9b24e1635f2c2360ec09e92763a9163a0a531
top mac <topmac@192.168.1.2>
2026-01-01 05:08:37 +0100

    deploy check 2

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,12 +2,13 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import "../globals.css";
 
-type Props = {
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
-};
-
-export default async function LocaleLayout({ children, params }: Props) {
+}) {
   const { locale } = await params;
 
   const messages = await getMessages({ locale });
